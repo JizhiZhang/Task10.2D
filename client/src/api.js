b485a1813b79f52132d0ae7a48feb1a3e26531f0
e <zhangjizhi0558@gmail.com>
2020-10-11 09:41:06 +0800

    change

--- client/src/api.js
+++
@@ -1,3 +0,0 @@
-export const getTodos = () => fetch("http://localhost:8000/getAll").then(res=>res.json())
-
-export const getTodo = (id) => fetch(`http://localhost:8000/${id}`).then(res => res.json())
